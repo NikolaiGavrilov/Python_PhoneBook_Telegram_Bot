83f6e9a04eb97fab9e36d905b13e7259c4f550fd
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-09-03 21:22:17 +0300

    Added quick buttons

--- Phonebook_TG_bot.py
+++ Phonebook_TG_bot.py
@@ -1,11 +1,13 @@
 #Ссылка на бота: t.me/Phone_Book_Gavrilov_bot
 
 import telebot
+from telebot import types
 from random import *
 import json
 import requests
+import config
 
-phonebook = [["Дядя Петя", "Челябинск", "12.12.1973", "12346"], ["Тётя Таня", "Сергиев Посад", "11.11.1974", "202346"]]
+phonebook = [["Дядя Петя", "Челябинск", "12.12.1973", "12-34-6"], ["Тётя Таня", "Сергиев Посад", "11.11.1974", "20-23-46"], ["Настя", "Санкт-Петербург", "15.05.1997", "412-24-26"], ["Харис Абдрахманович", "Таганрог", "13.01.1965", "8-800-555-35-35"]]
 
 API_TOKEN='6594391523:AAFTGzLwGtadT87OLhQzIUlVRbMSkM5mX4A'
 bot = telebot.TeleBot(API_TOKEN)
@@ -16,12 +18,46 @@ def load_all_contacts():
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, "Welcome to your contact list!")
-    bot.send_message(message.chat.id, "You can use following commands in this bot:\n /start - to launch the bot\n /stop - to stop the bot\n /all - to show all contacts in your phone book\n /add - to add new contact\n /save - to save the progress you've achieved\n /full - to learn the contact's full information by inputing his or her name\n /remove - to remove the contact from your list\n /edit - to rewrite the contact's information")
-phonebook = load_all_contacts()
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("📕 All")
+    btn2 = types.KeyboardButton("✔️ Add")
+    btn3 = types.KeyboardButton("🔎 Info")
+    btn4 = types.KeyboardButton("📝 Edit")
+    btn5 = types.KeyboardButton("❌ Remove")
+    btn6 = types.KeyboardButton("👌 Save")
+    btn7 = types.KeyboardButton("👋 Stop")
+    markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7)
+    bot.send_message(message.chat.id, "Welcome to your contact list!\nYou can use following commands in this bot:\n /start - to launch the bot\n /stop - to stop the bot\n /all - to show all contacts in your phone book\n /add - to add new contact\n /save - to save the progress you've achieved\n /full - to learn the contact's full information by inputing his or her name\n /remove - to remove the contact from your list\n /edit - to rewrite the contact's information\n Or just use buttons following the instruction given below 😎".format(message.from_user), reply_markup=markup)
+    bot.send_photo(message.chat.id, open('instruction.jpg', 'rb'))
+load_all_contacts()
+
+@bot.message_handler(content_types=['text'])
+def user_choice(message):
+    if (message.text == "📕 All") or (message.text == "/all"):
+        show_all(message)
+    elif (message.text == "✔️ Add") or (message.text == "/add"):
+        get_data(message)
+    elif (message.text == "🔎 Info") or (message.text == "/full"):
+        learn_about_contact(message)
+    elif (message.text == "📝 Edit") or (message.text == "/edit"):
+        find_contact_to_edit(message)
+    elif (message.text == "❌ Remove") or (message.text == "/remove"):
+        find_contact_to_remove(message)
+    elif (message.text == "👌 Save") or (message.text == "/save"):
+        save_contacts(message)
+    elif (message.text == "👋 Stop") or (message.text == "/stop"):
+        stop_message(message)
+
+#Старая версия старта
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     bot.send_message(message.chat.id, "Welcome to your contact list!")
+#     bot.send_message(message.chat.id, "You can use following commands in this bot:\n /start - to launch the bot\n /stop - to stop the bot\n /all - to show all contacts in your phone book\n /add - to add new contact\n /save - to save the progress you've achieved\n /full - to learn the contact's full information by inputing his or her name\n /remove - to remove the contact from your list\n /edit - to rewrite the contact's information")
+# phonebook = load_all_contacts()
+
 
 @bot.message_handler(commands=['stop'])
-def start_message(message):
+def stop_message(message):
     bot.send_message(message.chat.id, "You closed your contact list. Bye!")
 
 @bot.message_handler(commands=['all'])
@@ -33,8 +69,8 @@ def show_all(message):
 
 @bot.message_handler(commands=['add'])
 def get_data(message):
-    string_to_remember = bot.send_message(message.chat.id, "Input name, city, birthday, and phone number of your contact by writing them in one line using ',' as separator between words:")
-    bot.register_next_step_handler(string_to_remember, add_data)
+    message = bot.send_message(message.chat.id, "Input name, city, birthday, and phone number of your contact by writing them in one line using ',' as separator between words:")
+    bot.register_next_step_handler(message, add_data)
 
 def add_data(message):
     text = message.text.split(",")
@@ -54,8 +90,8 @@ def save_contacts(message):
 
 @bot.message_handler(commands=['full'])
 def learn_about_contact(message):
-    contact_to_input = bot.send_message(message.chat.id, "Input name of the contact to learn full infomation about him or her:")
-    bot.register_next_step_handler(contact_to_input, find_phone)
+    message = bot.send_message(message.chat.id, "Input name of the contact to learn full infomation about him or her:")
+    bot.register_next_step_handler(message, find_phone)
 
 def find_phone(message):
     contact_to_find = message.text 
@@ -66,8 +102,8 @@ def find_phone(message):
 
 @bot.message_handler(commands=['remove'])
 def find_contact_to_remove(message):
-    contact_to_remove_found = bot.send_message(message.chat.id, "Input name of the contact to remove him or her from your book:")
-    bot.register_next_step_handler(contact_to_remove_found, remove_contact)
+    message = bot.send_message(message.chat.id, "Input name of the contact to remove him or her from your book:")
+    bot.register_next_step_handler(message, remove_contact)
 
 def remove_contact(message):
     contact_to_remove = message.text 
@@ -80,18 +116,18 @@ def remove_contact(message):
 
 @bot.message_handler(commands=['edit'])
 def find_contact_to_edit(message):
-    contact_to_edit_found = bot.send_message(message.chat.id, "Input name of the contact to edit:")
-    bot.register_next_step_handler(contact_to_edit_found, find_contact_position)
+    message = bot.send_message(message.chat.id, "Input name of the contact to edit:")
+    bot.register_next_step_handler(message, find_contact_position)
 
 def find_contact_position(message):
     contact_to_edit = message.text
     for i in range(len(phonebook)):
         contact = phonebook[i]
         if contact[0] == contact_to_edit:
-            contact_to_edit_found = bot.send_message(message.chat.id, f"Current data is \nName:{contact[0]} \nCity:{contact[1]} \nBirthday:{contact[2]} \nPhone number:{contact[3]}. \nInput new data in one line separating each new word with ',':")
+            message = bot.send_message(message.chat.id, f"Current data is \nName:{contact[0]} \nCity:{contact[1]} \nBirthday:{contact[2]} \nPhone number:{contact[3]}. \nInput new data in one line separating each new word with ',':")
             phonebook.remove(phonebook[i])
             break
-    bot.register_next_step_handler(contact_to_edit_found, rewrite_contact_info)
+    bot.register_next_step_handler(message, rewrite_contact_info)
 
 def rewrite_contact_info(message):
     text = message.text.split(",")
